07aa49359d2053be5ad403beb9b92118a12b591d
hiranya911 <hiranya911@gmail.com>
2018-06-07 12:24:57 -0700

    Add movie logic and improved error handling

--- node/admin-service/src/app.ts
+++ node/admin-service/src/app.ts
@@ -7,7 +7,7 @@ class HttpError extends Error {
   constructor(
     message: string,
     public readonly status: number,
-    public readonly details?: any) {
+    public readonly details?: Error) {
     super(message);
     Object.setPrototypeOf(this, HttpError.prototype);
   }
@@ -46,6 +46,22 @@ app.get('/movies/:movieId', async (req, res, next) => {
   }
 });
 
+app.post('/movies', async (req, res, next) => {
+  let movie: fireflicks.Movie;
+  try {
+    movie = fireflicks.newMovie(req.body);
+  } catch (err) {
+    return next(new HttpError('Failed to parse request', 400, err));
+  }
+
+  try {
+    const id = await fireflicks.addMovie(movie);
+    res.json({id});
+  } catch (err) {
+    return next(new HttpError('Failed to save movie', 500, err));
+  }
+});
+
 app.post('/moderators', async (req, res, next) => {
   const body = req.body || {};
   if (!body.email) {
@@ -65,12 +81,14 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
   let details: any;
   if (err instanceof HttpError) {
     status = err.status;
-    details = err.details;
+    if (err.details) {
+      details = err.details.message;
+    }
   }
   res.status(status);
   res.json({
-    details,
     message: err.message,
+    details,
   });
 });
 
