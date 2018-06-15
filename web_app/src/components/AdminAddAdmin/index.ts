d38b8dcf9be9bc21717cf9854f287f2cfebcb020
Jen Person <personjeh@gmail.com>
2018-06-15 14:03:40 -0700

    added license

--- web_app/src/components/AdminAddAdmin/index.ts
+++ web_app/src/components/AdminAddAdmin/index.ts
@@ -1,19 +1,22 @@
+// Copyright 2018 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // VueJS modules
 import Vue from "vue";
 import { Component, Inject, Model, Prop, Watch } from "vue-property-decorator";
 import Toolbar from "../Toolbar";
 import FirebaseSingleton from "../../services/FirebaseSingleton";
-
-type Movie = {
-  title: string;
-  averageRating: number;
-  overview: string;
-  poster: string;
-  genres: {string: boolean};
-  ratigsCount: number;
-  //genreList: string;
-  //key: string;
-};
 
 @Component({
   components: {
@@ -21,11 +24,10 @@ type Movie = {
   }
 })
 export default class AdminAddAdmin extends Vue {
+  base_url = "http://localhost:3000/";
   fst: FirebaseSingleton;
   userId: string;
   hide = true;
-
-  base_url = "https://fireflicks-io.appspot.com/"
   user_email = "";
   async mounted() {
     await System.import("isomorphic-fetch");
--- web_app/src/components/Toolbar/index.ts
+++ web_app/src/components/Toolbar/index.ts
@@ -1,22 +1,24 @@
+// Copyright 2018 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // VueJS modules
 import Vue from "vue";
 import { Component, Inject, Model, Prop, Watch } from "vue-property-decorator";
-import App from "../App";
 import FirebaseSingleton from "../../services/FirebaseSingleton";
-import { Http2ServerResponse } from "http2";
 import DataModel from "../../services/DataModel";
 System.import("isomorphic-fetch")
 require("isomorphic-fetch");
-
-// type Movie = {
-//   title: string;
-//   averageRating: number;
-//   overview: string;
-//   poster: string;
-//   genres: {string: boolean};
-//   genreList: string;
-//   key: string;
-// };
 
 @Component({
 })
